a6d947689f5cd394e871fa2d3ebb1745ed5a571e
Sasha Szpakowski <slime73@gmail.com>
2026-07-26 14:46:20 -0300

    vulkan: hack: don't allow creating cubemap views of array textures

--- src/modules/graphics/vulkan/Texture.cpp
+++ src/modules/graphics/vulkan/Texture.cpp
@@ -117,7 +117,7 @@ bool Texture::loadVolatile()
 			}
 		}
 
-		if (texType == TEXTURE_CUBE || (texType == TEXTURE_2D_ARRAY && layerCount >= 6 && pixelWidth == pixelHeight))
+		if (texType == TEXTURE_CUBE)
 			createFlags |= VK_IMAGE_CREATE_CUBE_COMPATIBLE_BIT;
 
 		msaaSamples = vgfx->getMsaaCount(requestedMSAA);
